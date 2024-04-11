adbdefa3f60af78669d5712d17c73e29802f037f
melosh <milasholsting@protonmail.com>
2024-04-11 21:52:41 +0200

    add types to circum-icons-svelte
    i modified it to use sveltekit instead of rollup for simplicity

---
+++ packages/circum-icons-svelte/src/lib/svelte/CircumIcons.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import type { SvelteComponentTyped } from "svelte";
+	import { icons, type IconNames } from "$lib/iconList.js";
+	export {clasName as class}
+	export {idName as id}
+	export let name: IconNames;
+	export let size = "32px";
+	export let color = "currentColor";
+	const findIcon = () => {
+		const i = icons.find((e) => e.name === name)
+		if(i === undefined) {
+			throw new Error("unknown icon");
+		}
+		return i;
+	}
+	const displayIcon = findIcon();
+
+</script>
+
+<svg
+	class="circumicon__{name}"
+	width={size}
+	height={size}
+	viewBox="0 0 24 24"
+	fill={color}
+>
+	{@html displayIcon.svg? displayIcon.svg : "error: unknown icon"}
+</svg>
