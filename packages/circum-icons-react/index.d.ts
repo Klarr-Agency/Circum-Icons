1382631c3abeed7d30d2b773dad3e35ef1fcf4f2
melosh101 <milasholsting@protonmail.com>
2024-04-11 14:26:54 +0200

    add types to react component lib

---
+++ packages/circum-icons-react/index.d.ts
@@ -0,0 +1,11 @@
+import type { Component } from "react"
+
+type IconProps = { 
+    name: string
+    size?: string
+    color?: string
+    displayIcon?: string
+}
+
+
+export default function Icon(props: IconProps): Component
