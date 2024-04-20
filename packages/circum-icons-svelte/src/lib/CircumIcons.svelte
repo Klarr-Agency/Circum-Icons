198acaf760b54c26619418006d784fe362f610cb
Guillaume_Bernier <guillaume.bernier@klarr.ca>
2024-04-20 12:31:52 -0400

    format document

--- packages/circum-icons-svelte/src/lib/CircumIcons.svelte
+++ packages/circum-icons-svelte/src/lib/CircumIcons.svelte
@@ -1,20 +1,18 @@
 <script lang="ts">
-	import type { SvelteComponentTyped } from "svelte";
 	import { icons, type IconNames } from "$lib/iconList.js";
-	export {clasName as class}
-	export {idName as id}
+	export { clasName as class };
+	export { idName as id };
 	export let name: IconNames;
 	export let size = "32px";
 	export let color = "currentColor";
 	const findIcon = () => {
-		const i = icons.find((e) => e.name === name)
-		if(i === undefined) {
+		const i = icons.find((e) => e.name === name);
+		if (i === undefined) {
 			throw new Error("unknown icon");
 		}
 		return i;
-	}
+	};
 	const displayIcon = findIcon();
-
 </script>
 
 <svg
@@ -24,5 +22,5 @@
 	viewBox="0 0 24 24"
 	fill={color}
 >
-	{@html displayIcon.svg? displayIcon.svg : "error: unknown icon"}
+	{@html displayIcon.svg ? displayIcon.svg : "error: unknown icon"}
 </svg>
